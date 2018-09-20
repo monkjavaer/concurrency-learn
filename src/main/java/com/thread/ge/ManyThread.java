10aee28819f2c413c8ea53724cddb70464cb2e4a
lighter <1269106963@qq.com>
2018-09-20 18:14:08 +0800

    init

---
+++ src/main/java/com/thread/ge/ManyThread.java
@@ -0,0 +1,14 @@
+package com.thread.ge;
+
+/**
+ * 创建100个线程，等待，测试cpu,内存使用情况
+ * @author tangquanbin
+ * @date 2018/9/20 13:16
+ */
+public class ManyThread {
+
+    public static void main(String[] args) {
+
+    }
+
+}
