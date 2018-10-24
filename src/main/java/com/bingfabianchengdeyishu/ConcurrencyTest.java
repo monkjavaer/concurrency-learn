33c997fd110ebedccb1038bc115bb6f94f5ed35b
lighter <1269106963@qq.com>
2018-10-24 19:32:38 +0800

    thread 状态

--- src/main/java/com/bingfabianchengdeyishu/ConcurrencyTest.java
+++ src/main/java/com/bingfabianchengdeyishu/ConcurrencyTest.java
@@ -3,12 +3,13 @@ package com.bingfabianchengdeyishu;
 /**
  * 并发和单线程执行测试
  * 创建线程和上下文切换有开销
+ * count大于100w甚至更大时，并发会有效果
  * @author tangquanbin
  * @date 2018/10/24 11:25
  */
 public class ConcurrencyTest {
     /** 执行次数 */
-    private static final long count = 100000;
+    private static final long count = 10000000;
 
     public static void main(String[] args) throws InterruptedException {
         //并发计算
---
+++ src/main/java/com/bingfabianchengdeyishu/Priority.java
@@ -0,0 +1,58 @@
+package com.bingfabianchengdeyishu;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 线程优先级不能作为程序正确性的依赖，因为操作系统可以完全不用理会Java
+ * 线程对于优先级的设定。
+ * @author tangquanbin
+ * @date 2018/10/24 18:53
+ */
+public class Priority {
+    private static volatile boolean notStart = true;
+    private static volatile boolean notEnd   = true;
+
+    public static void main(String[] args) throws Exception {
+        List<Job> jobs = new ArrayList<Job>();
+        for (int i = 0; i < 10; i++) {
+            int priority = i < 5 ? Thread.MIN_PRIORITY : Thread.MAX_PRIORITY;
+            Job job = new Job(priority);
+            jobs.add(job);
+            Thread thread = new Thread(job, "Thread:" + i);
+            thread.setPriority(priority);
+            thread.start();
+        }
+        notStart = false;
+        Thread.currentThread().setPriority(8);
+        System.out.println("done.");
+        TimeUnit.SECONDS.sleep(10);
+        notEnd = false;
+
+        for (Job job : jobs) {
+            System.out.println("Job Priority : " + job.priority + ", Count : " + job.jobCount);
+        }
+
+    }
+
+    static class Job implements Runnable {
+        private int  priority;
+        private long jobCount;
+
+        public Job(int priority) {
+            this.priority = priority;
+        }
+
+        @Override
+        public void run() {
+            while (notStart) {
+                Thread.yield();
+            }
+            while (notEnd) {
+                Thread.yield();
+                jobCount++;
+            }
+        }
+    }
+}
