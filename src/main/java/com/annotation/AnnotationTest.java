d50c1b70f231bf8be34195204680ce8d4d981db9
lighter <1269106963@qq.com>
2018-10-31 22:20:46 +0800

    lock

---
+++ src/main/java/com/annotation/AnnotationTest.java
@@ -0,0 +1,16 @@
+package com.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 自定义注解
+ * 注解的可用的类型包括以下几种：所有基本类型、String、Class、enum、Annotation、以上类型的数组形式。
+ * @author tangquanbin
+ * @date 2018/10/31 19:28
+ */
+@Target({ElementType.METHOD,ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface AnnotationTest {
+    
+}
