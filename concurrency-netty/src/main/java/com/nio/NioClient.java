61856efb8185a71cfe15cfc6160ac4fdd4e1ea8c
lighter <1269106963@qq.com>
2019-07-18 14:44:04 +0800

    NIO客户端

--- concurrency-netty/src/main/java/com/nio/NioClient.java
+++ concurrency-netty/src/main/java/com/nio/NioClient.java
@@ -1,10 +1,38 @@
 package com.nio;
 
+import com.netty.decodeandencode.server.CreateUnvProtocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
 /**
  * @author monkjavaer
- * @version V1.0
- * @date 2019/7/15 0015 21:49
+ * @date 2019/7/18 13:05
  */
 public class NioClient {
+    private static Logger LOGGER = LoggerFactory.getLogger(CreateUnvProtocol.class);
+    private static int PORT = 9011;
+    private static String[] messages = {"这是服务器"};
+
+    public static void main(String[] args) {
+        try {
+            SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress(InetAddress.getLocalHost(), PORT));
+            for (String msg : messages) {
+                ByteBuffer myBuffer = ByteBuffer.allocate(1024);
+                myBuffer.put(msg.getBytes());
+                myBuffer.flip();
+                socketChannel.write(myBuffer);
+            }
+            LOGGER.info("Closing Client connection...");
+            socketChannel.close();
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
 
 }
--- concurrency-netty/src/main/java/com/nio/NioServer.java
+++ concurrency-netty/src/main/java/com/nio/NioServer.java
@@ -1,13 +1,18 @@
 package com.nio;
 
+import com.netty.decodeandencode.server.CreateUnvProtocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -16,46 +21,87 @@ import java.util.Set;
  * @version V1.0
  * @date 2019/7/15 0015 21:49
  */
-public class NioServer implements Runnable{
+public class NioServer implements Runnable {
+    private static Logger LOGGER = LoggerFactory.getLogger(CreateUnvProtocol.class);
+    @Override
+    public void run() {
+        try {
+            //1、打开ServerSocketChannel，监听客户端的链接
+            ServerSocketChannel serverSocket = ServerSocketChannel.open();
+            //2、绑定监听端口,设置backlog（默认50）:请求传入连接队列的最大长度
+            serverSocket.socket().bind(new InetSocketAddress(9011), 1024);
+            //3、false,设置为非阻塞模式
+            serverSocket.configureBlocking(false);
+            //4、创建Selector，Selector是NIO的多路复用器，Selector会不断轮询注册在它上面的通道Channel,
+            //找出就绪状态的Channel(Channel通道发生读、写事件)。
+            Selector selector = Selector.open();
+            //5、注册通道Channel到多路复用器Selector，并说明关注点SelectionKey.OP_ACCEPT，监听ACCEPT事件
+            serverSocket.register(selector, SelectionKey.OP_ACCEPT);
+            LOGGER.info("Listening on port {}" , 9011);
 
-        @Override
-        public void run() {
-            try {
-                //1、打开ServerSocketChannel，监听客户端的链接
-                ServerSocketChannel serverSocket = ServerSocketChannel.open();
-                //2、绑定监听端口
-                serverSocket.bind(new InetSocketAddress(InetAddress.getLocalHost(),8888));
-                //3、false,设置为非阻塞模式
-                serverSocket.configureBlocking(false);
-                //4、创建Selector，Selector是NIO的多路复用器，Selector会不断轮询注册在它上面的通道Channel,
-                //找出就绪状态的Channel(Channel通道发生读、写事件),
-                Selector selector = Selector.open();
-                //5、注册通道Channel到多路复用器Selector，并说明关注点SelectionKey.OP_ACCEPT，监听ACCEPT事件
-                serverSocket.register(selector, SelectionKey.OP_ACCEPT);
-                //6、Selector轮询就绪的Channel
-                while (true) {
-                    // 阻塞等待就绪的 Channel，这是关键点之一
-                    selector.select();
-                    Set<SelectionKey> selectedKeys = selector.selectedKeys();
-                    Iterator<SelectionKey> iter = selectedKeys.iterator();
-                    while (iter.hasNext()) {
-                        SelectionKey key = iter.next();
-                        if (key.isValid()) {
-                            if (key.isAcceptable()) {
-                                //SelectionKey可以获取就绪状态的Channel
-                                ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
-                                //7、多路复用器Selector监听到有新的客户端连接，完成TCP三次握手建立连接。
-                                SocketChannel clientSocketChannel = serverSocketChannel.accept();
-                                //8、设置客户端SocketChannel为非阻塞模式
-                                clientSocketChannel.configureBlocking(false);
-                                clientSocketChannel.write(Charset.defaultCharset().encode("Hello world!"));
+            //6、Selector轮询就绪的Channel
+            while (true) {
+                // 阻塞等待就绪的 Channel，这是关键点之一
+                //selector1秒被唤醒
+                int n = selector.select(1000);
+                if (n == 0) {
+                    continue;
+                }
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iter = selectedKeys.iterator();
+                while (iter.hasNext()) {
+                    SelectionKey key = iter.next();
+                    if (key.isValid()) {
+                        if (key.isAcceptable()) {
+                            //SelectionKey可以获取就绪状态的Channel
+                            ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+                            //7、多路复用器Selector监听到有新的客户端连接，完成TCP三次握手建立连接。
+                            SocketChannel clientSocketChannel = serverSocketChannel.accept();
+                            //8、设置客户端SocketChannel为非阻塞模式
+                            clientSocketChannel.configureBlocking(false);
+                            //9、注册加入新的通道OP_READ
+                            clientSocketChannel.register(selector, SelectionKey.OP_READ);
+                        }
+
+                        //读取客户端数据
+                        //if(key.isReadable())等价于if((key.readyOps( ) & SelectionKey.OP_READ) != 0)
+                        if (key.isReadable()) {
+                            SocketChannel socketChannel = (SocketChannel) key.channel();
+                            //创建buffer
+                            ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                            int readPosition = socketChannel.read(readBuffer);
+                            if (readPosition > 0) {
+                                //flip()方法，Buffer从写模式切换到读模式,将limit设置为position，position设为0。
+                                readBuffer.flip();
+                                byte[] bytes = new byte[readBuffer.remaining()];
+                                //从可读buffer中读取数据
+                                readBuffer.get(bytes);
+                                LOGGER.info("接收客户端发送消息：{}" , new String(bytes, StandardCharsets.UTF_8));
+
+                                byte[] sendBytes = "server 收到".getBytes();
+                                ByteBuffer writeBuffer = ByteBuffer.allocate(1024);
+                                writeBuffer.flip();
+                                //put 向buffer添加元素
+                                writeBuffer.put(sendBytes);
+                                socketChannel.write(writeBuffer);
+                            }
+
+                            if (readPosition < 0) {
+                                // Close channel on EOF, invalidates the key
+                                key.cancel();
+                                socketChannel.close();
                             }
                         }
-                        iter.remove();
                     }
+                    iter.remove();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        new Thread(new NioServer()).start();
+    }
 }
